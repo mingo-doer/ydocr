aed73f8e0a90881ccf09134246f5dedc28a83ec5
mingo-doer <505991554@qq.com>
2022-08-31 19:58:42 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+with open('requirements.txt', 'r', encoding='utf-8') as f:
+    requirements = f.readlines()
+
+setuptools.setup(
+    name="ydocr",
+    version="1.0",
+    author="jiaer",
+    author_email="jia.er@winrobot360.com",
+    license='Apache 2.0',
+    description="离线ocr",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/triwinds/ppocr-onnx",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+)
